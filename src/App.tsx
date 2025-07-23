095748add2c345c47985002342e1353f770606c6
nurilanwaralabsory <nurilanwaralabsory@gmail.com>
2025-07-23 18:31:45 +0700

    feat: completed task on click button toggle checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,16 @@ import React, { useEffect, useState } from "react";
 import { Badge } from "./components/ui/badge";
 import { Card, CardContent } from "./components/ui/card";
 
+interface Task {
+  text: string;
+  completed: boolean;
+}
+
 function App() {
   const [inputTask, setInputTask] = useState("");
 
   // function yang diberikan ke dalam useState disebut LAZY INITIAL STATE. React akan menjalankan fungsi ini hanya 1x, yaitu saat komponen pertama kali di render untuk menentukan nilai awal dari state
-  const [tasks, setTasks] = useState<string[]>(() => {
+  const [tasks, setTasks] = useState<Task[]>(() => {
     const savedTasks = localStorage.getItem("tasks");
     return savedTasks ? JSON.parse(savedTasks) : [];
   });
@@ -27,8 +32,20 @@ function App() {
     e.preventDefault();
     if (!inputTask.trim()) return;
 
-    setTasks([...tasks, inputTask]);
+    setTasks([{ text: inputTask, completed: false }, ...tasks]);
     setInputTask("");
+  };
+
+  const handleToggleTask = (taskIndex: number) => {
+    const newTasks = tasks.map((task, index) => {
+      if (index === taskIndex) {
+        return { ...task, completed: !task.completed };
+      }
+
+      return task;
+    });
+
+    setTasks(newTasks);
   };
 
   return (
@@ -40,6 +57,7 @@ function App() {
               className="bg-[#262626] h-12 border border-zinc-950 placeholder:text-gray-500 caret-[#1E6F9F] focus:outline-none focus:ring-[#1E6F9F] focus:border-none"
               placeholder="Add a new task"
               onChange={handleInputChange}
+              value={inputTask}
             />
             <Button className="bg-[#1E6F9F] text-white h-12 flex items-center cursor-pointer">
               New <PlusCircle />
@@ -52,7 +70,15 @@ function App() {
               Tugas dibuat{" "}
               <Badge className="bg-gray-600 ml-1">{tasks.length}</Badge>
             </p>
-            <p>Selesai</p>
+            <div>
+              Selesai{" "}
+              <Badge className="bg-gray-600 ml-1">
+                {" "}
+                {tasks.filter((task) => task.completed).length}
+              </Badge>{" "}
+              of
+              <Badge className="bg-gray-600 ml-1"> {tasks.length}</Badge>
+            </div>
           </div>
           {tasks.length > 0 ? (
             <ul className="flex flex-col space-y-2 mt-4">
@@ -63,9 +89,18 @@ function App() {
                       <div className="flex items-center gap-x-4">
                         <input
                           type="checkbox"
+                          onChange={() => handleToggleTask(index)}
                           className="appearance-none h-6 w-6 shrink-0 border-2 border-[#4EA8DE] rounded-full cursor-pointer bg-center  checked:bg-[#5E60CE] checked:border-0 checked:bg-[url('data:image/svg+xml,%3csvg%20viewBox%3d%220%200%2016%2016%22%20fill%3d%22white%22%20xmlns%3d%22http%3a//www.w3.org/2000/svg%22%3e%3cpath%20d%3d%22M12.207%204.793a1%201%200%20010%201.414l-5%205a1%201%200%2001-1.414%200l-2-2a1%201%200%20011.414-1.414L6.5%209.086l4.293-4.293a1%201%200%20011.414%200z%22/%3e%3c/svg%3e')]"
                         />
-                        <p className={`text-white ${index}`}>{task}</p>
+                        <p
+                          className={` ${
+                            task.completed
+                              ? "line-through text-gray-400"
+                              : "text-white"
+                          }`}
+                        >
+                          {task.text}
+                        </p>
                       </div>
                     </CardContent>
                   </Card>
